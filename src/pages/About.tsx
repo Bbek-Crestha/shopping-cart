c42f6697020bbba32e6a77329d9de8b4f6cb5a7c
Bbek-Crestha <bcrestha07@gmail.com>
2022-07-01 14:36:14 +0545

    feat: create buttons to navigate to store page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,13 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const About = () => {
 	return (
 		<div>
 			<h1>About</h1>
+			<Link to="/store">
+				<Button>Go to Store &rarr;</Button>
+			</Link>
 		</div>
 	);
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const Home = () => {
 	return (
 		<div>
 			<h1>Home</h1>
+			<Link to="/store">
+				<Button>Go to Store &rarr;</Button>
+			</Link>
 		</div>
 	);
 };
