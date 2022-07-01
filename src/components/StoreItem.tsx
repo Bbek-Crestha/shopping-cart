a8258f323c7d1e6a07f75ff050b3c14b491c1c30
Bbek-Crestha <bcrestha07@gmail.com>
2022-07-01 12:55:02 +0545

    feat: create item card for all items in store page

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,34 @@
+import { Card } from "react-bootstrap";
+
+import formatCurrency from "../utilities/formatCurrency";
+
+type StoreItemProps = {
+	id: number;
+	name: string;
+	price: number;
+	imgUrl: string;
+};
+
+const StoreItem = (props: StoreItemProps) => {
+	const { id, name, price, imgUrl } = props;
+
+	return (
+		<Card>
+			<Card.Img
+				variant="top"
+				src={imgUrl}
+				height="200px"
+				style={{ objectFit: "cover" }}
+			/>
+
+			<Card.Body className="d-flex flex-column">
+				<Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+					<span className="fs-2">{name}</span>
+					<span className="ms-2 text-muted">{formatCurrency(price)}</span>
+				</Card.Title>
+			</Card.Body>
+		</Card>
+	);
+};
+
+export default StoreItem;
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,23 @@
+import { Col, Row } from "react-bootstrap";
+
+import StoreItem from "../components/StoreItem";
+import storeItems from "../data/Items.json";
+
 const Store = () => {
 	return (
 		<div>
 			<h1>Store</h1>
+
+			<Row md={2} xs={1} lg={3} className="g-3">
+				{storeItems.map((item) => {
+					const { id, name, price, imgUrl } = item;
+					return (
+						<Col key={id}>
+							<StoreItem {...item} />
+						</Col>
+					);
+				})}
+			</Row>
 		</div>
 	);
 };
